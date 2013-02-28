7e128b0d95da71c68b1debe1eb5d6de0a810aaa0
Luís Miguel Alves Fernandes <luisapidcloud@hotmail.com>
2013-02-28 23:10:04 +0000

    funciona :3 xD mais fácil do q parece

--- UmTesteParaRepositorio/UmTesteParaRepositorio/Form1.Designer.cs
+++ UmTesteParaRepositorio/UmTesteParaRepositorio/Form1.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnConfirmar = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Location = new System.Drawing.Point(90, 76);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(75, 23);
+            this.btnConfirmar.TabIndex = 0;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(557, 168);
+            this.Controls.Add(this.btnConfirmar);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +53,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnConfirmar;
     }
 }
 
